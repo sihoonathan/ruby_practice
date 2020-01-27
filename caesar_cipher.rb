4c713439b0799c6586607ea48ed1814b93a9c8ca
Nathan C <sihoonathan@gmail.com>
2020-01-27 09:12:20 -0800

    minor change

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,9 @@
 def caesar_cipher(original_text, num_shift)
   arr = original_text.split("")
-  i = 0
+  index = 0
   converted_text = ""
-  while i < arr.size
-    char_to_convert = arr[i]
+  while index < arr.size
+    char_to_convert = arr[index]
     if char_to_convert.match?(/[A-Z]/)
       converted_text += ("A".ord + (char_to_convert.ord - "A".ord + num_shift) % 26).chr
     elsif char_to_convert.match?(/[a-z]/)
@@ -11,7 +11,7 @@ def caesar_cipher(original_text, num_shift)
     else
       converted_text += char_to_convert
     end
-    i += 1
+    index += 1
   end
   return converted_text
 end
